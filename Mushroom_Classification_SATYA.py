cb60066a91afd8723fe26d6ad25d6501c3b6c8b7
bbcamo <96676901+bbcamo@users.noreply.github.com>
2024-08-19 12:06:52 +0900

    v2(0819)
    data preprocessing

--- Mushroom_Classification_SATYA.py
+++ Mushroom_Classification_SATYA.py
@@ -77,4 +77,70 @@ for col, unique_count in unique_values.items():
 
 # # Exploratory Data Analysis(EDA)
 
+import seaborn as sns
+
+# missing value ratio of data
+df_train.isna().mean()
+
+# more than 10%
+df_train.isna().mean() > 0.10
+
+# filtering as condition
+df_train.isna().mean()[df_train.isna().mean() > 0.10]
+
+# +
+missing_train = df_train.isna().mean() * 100
+missing_test = df_test.isna().mean() * 100
+
+print('Columns in df_train with more than 10% missing values:')
+print(missing_train[missing_train > 10])
+
+print('\n Columns in df_test with more than 10% missing values:')
+print(missing_test[missing_test > 10])
+# -
+
+# > Both df_train and df_test share the same columns with more than 10% missing values. <br>
+# > Stem_root, veil-type, veil-color, and spore-print-color have a lot of missing values, excluding 80% in both datasets. <br>
+# > Cap-surface, gill-attachment, gill-spacing, and stem-sureface have moderate missing values(16% ~ 64%).
+
+# +
+# visualization
+missing_values = df_train.isna().mean() * 100
+missing_values = missing_values[missing_values > 0]
+missing_values = missing_values.sort_values(ascending=False)
+
+#missing_values.index
+
+plt.figure(figsize=(10,6))
+sns.barplot(x=missing_values.index, y=missing_values.values, hue=missing_values.index)
+plt.xticks(rotation=90)
+plt.xlabel('Features')
+plt.ylabel('Percentage of Missing Values')
+plt.title('Missing Values Distribution of df_train')
+plt.show()
+# -
+
+# !pip install dython
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
+import category_encoders as ce
+# +
+# drop high missing columns
+missing_threshold = 0.95
+high_missing_columns = df_train.columns[df_train.isna().mean() > missing_threshold]
+
+df_train = df_train.drop(columns=high_missing_columns)
+df_test = df_test.drop(columns=high_missing_columns)
+
+
+# +
+# target = 'class'
+# -
+
+# 17 columns have missing value at leat one
+for column in df_train.columns:
+    if df_train[column].isna().any():
+        print(column)
+
 
